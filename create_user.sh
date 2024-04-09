51fa0527037034a71b921a90cdd7ef60f30abf89
Lordsniffer22 <128750702+Lordsniffer22@users.noreply.github.com>
2024-04-10 01:21:24 +0300

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -8,12 +8,10 @@ limite=$4
 dataValidade=$(date "+%Y-%m-%d" -d "+$validade days")
 password=$(perl -e 'print crypt($ARGV[0], "password")' $senha)
 
-/usr/sbin/useradd -M -s /bin/false -e $dataValidade -p $password $usuario
+useradd -M -s /bin/false -e $dataValidade -p $password -c "$4, $2" $usuario
 
 if [ $? -eq 0 ]; then
-    
-    echo "usuario criado"
+    echo "User created"
     echo "$senha" > /etc/SSHPlus/senha/$usuario
     echo "$usuario $limite" >> /root/usuarios.db
-    
-    fi
+fi
